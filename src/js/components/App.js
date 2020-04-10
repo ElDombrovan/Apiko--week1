ed6a5f66549e2b7001338d5e737b936a3e3eb17c
El Dombrovan <el.dombrovan@gmail.com>
2020-04-10 13:26:12 +0300

    Add App component with test data

---
+++ src/js/components/App.js
@@ -0,0 +1,28 @@
+import { React } from '../dom/dom.js';
+
+export const App = React.createElement('div', {
+    attributes: {
+        style: 'background-color: red',
+    },
+    children: [
+        React.createElement('span', {
+            attributes: {},
+            children: 'Test text for span tag'
+        }),
+        React.createElement(' div', {
+            attributes: {
+                style: 'padding: 5px',
+                style: 'border: 1px solid white'
+            },
+            children: {
+                React.createElement('p', {
+                   attributes: {
+                       style: 'color: white',
+                       style: 'text-decoration: underline'
+                   },
+                   children: 'Paragraph tag with div parent' 
+                })
+            }
+        })
+    ]
+});
